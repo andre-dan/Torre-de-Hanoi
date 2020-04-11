2a45986c64d25eea112dee55c83d4e4e3f093e23
Andre Torres <andrerndc@gmail.com>
2020-04-11 00:25:31 -0300

    Correçao, envio de arquivo

--- towerHanoi.rb
+++ towerHanoi.rb
@@ -1,15 +1,19 @@
 def towerHanoi(disks, source=[], aux=[], target=[], moves=0)
- 
+  return 0 unless disks > 0
   if source.empty? && target.empty? && aux.empty?
     disks.times.reverse_each {|i| source << i+1}
   end
-       
-  if disks == 1 
+
+  if disks == 1
     target << source.pop
     moves+=1
   else
     moves = towerHanoi(disks - 1, source, target, aux, moves)
     moves = towerHanoi(1, source, aux, target, moves)
-    towerHanoi(disks - 1, aux, source, target, moves)         
+    towerHanoi(disks - 1, aux, source, target, moves)
   end
 end
+
+puts "towerHanoi(3) -> #{towerHanoi(3)}"
+puts "towerHanoi(5) -> #{towerHanoi(5)}"
+puts "towerHanoi(0) -> #{towerHanoi(0)}"
